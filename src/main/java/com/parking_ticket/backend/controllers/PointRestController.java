0f3fa4acc1719f0f1783730ac701d56c0c5143d3
ColdMold <44824348+ColdMold@users.noreply.github.com>
2019-11-24 12:21:59 -0600

    Update PointRestController.java
    added validation to post points

--- src/main/java/com/parking_ticket/backend/controllers/PointRestController.java
+++ src/main/java/com/parking_ticket/backend/controllers/PointRestController.java
@@ -16,51 +16,62 @@ import java.util.*;
 @Validated
 public class PointRestController {
 	@Autowired
-    private PointRepository repository;
+	private PointRepository repository;
 
-	//return list of all points
+	// return list of all points
 	@RequestMapping(value = "/points/", method = RequestMethod.GET)
-	public ResponseEntity<?> getAllPoints()
-	{
+	public ResponseEntity<?> getAllPoints() {
 		List<Point> inv = repository.findAll();
-		if(inv == null) {
+		if (inv == null) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-		else {
-			return new ResponseEntity<>(inv,HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(inv, HttpStatus.OK);
 		}
 	}
 
-	//return list of all points within two times
+	// return list of all points within two times
 	@RequestMapping(value = "/points", method = RequestMethod.GET)
-	public ResponseEntity<?> findByAllPointsWithinTimes(@RequestParam long time1, @RequestParam long time2)
-	{
+	public ResponseEntity<?> findByAllPointsWithinTimes(@RequestParam long time1, @RequestParam long time2) {
 		List<Point> inv = repository.findAll();
-		if(inv == null) {
+		if (inv == null) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-		else {
-			for(Iterator<Point> iter = inv.iterator(); iter.hasNext();) {
+		} else {
+			for (Iterator<Point> iter = inv.iterator(); iter.hasNext();) {
 				Point a = iter.next();
-				if(a.gettime() < time1 || a.gettime() > time2) {
+				if (a.gettime() < time1 || a.gettime() > time2) {
 					iter.remove();
 				}
 			}
-			return new ResponseEntity<>(inv,HttpStatus.OK);
+			return new ResponseEntity<>(inv, HttpStatus.OK);
 		}
 	}
-	
-	//create new point
-	@RequestMapping(value = "/point/", method = RequestMethod.POST, produces="application/json")
+
+	// create new point
+	@RequestMapping(value = "/point/", method = RequestMethod.POST, produces = "application/json")
 	public ResponseEntity<?> savePoint(@Valid @RequestBody Point savePoint) {
-		//TODO: Add validation here, Make sure that Point is not null and the coords and time is valid.
-		if(savePoint == null){
+		// TODO: Add validation here, Make sure that Point is not null and the coords
+		// and time is valid.
+		/*
+		 * rough area of campus and lots latitude: 36.051097 to 36.078917 longitude:
+		 * -94.187327 to -94.157985
+		 */
+		if (savePoint == null) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 
+		else if ((savePoint.getlat() < 36.051097) || (savePoint.getlat() > 36.078917)) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+		else if ((savePoint.getlon() < -94.187327) || (savePoint.getlon() > -94.157985)) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+		else if (savePoint.gettime() <= 0) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
 		return new ResponseEntity<>(repository.save(savePoint), HttpStatus.CREATED);
 	}
-
 
 }
